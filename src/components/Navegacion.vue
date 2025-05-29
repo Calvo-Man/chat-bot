2c44eb0fbf5eaaa8ec9779b343f180466c968c00
Miguel Angel Acuna <macunahernandez597@gmail.com>
2025-05-29 01:27:30 -0500

    comenzando

---
+++ src/components/Navegacion.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar color="primary" dark>
+    <v-app-bar-title>Chatbot</v-app-bar-title>
+    <v-spacer />
+    
+    <v-btn class="boton" text to="/acerca">Alimentos</v-btn>
+    <v-btn class="boton" text to="/contacto">Medio ambiente</v-btn>
+    <v-btn class="boton" text to="/contacto">Conocenos</v-btn>
+    <v-btn class="boton" text to="/contacto">Misión</v-btn>
+    <v-btn class="boton" text to="/contacto">Contacto</v-btn>
+    <v-btn class="boton" text to="/contacto">Chatbot</v-btn>
+  </v-app-bar>
+</template>
+<style scoped>
+.boton:hover {
+  
+  cursor: pointer;
+}
+</style>
