9d8a96bc607e177878a83196e372b665fa9bd199
Miguel Angel Acuna <macunahernandez597@gmail.com>
2025-06-06 00:12:52 -0500

    cambio

--- api/process.js
+++ api/process.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   console.log("🔄 Proxy llamado");
 
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
